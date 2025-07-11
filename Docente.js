22c6c3224dee6f35593d706ba9b09dc01e0f5706
Sofia Vaz <sofiavazdacostaxavier@gmail.com>
2025-07-11 13:30:01 -0300

    adicionando a classe Docente

---
+++ Docente.js
@@ -0,0 +1,18 @@
+// classe de Docentes 
+
+import User from "./User.js";
+
+class Docente extends  User {
+    constructor (nome, email, nascimento, role = "docente", ativo = "true") {
+        super(nome, email, nascimento, role, ativo); //superclasse - esses parametros estão definidos na superclasse de Admin, no caso User
+    }
+
+    aprovarEstudante (nomeEstudante, curso) {
+        return `O estudante ${nomeEstudante}, passou no curso de ${curso}, o responsável pelo curso é ${this.nome} `
+    }
+}
+
+const novoDocente = new Docente ('Ana','a@a.com', '2023-01-01');
+console.log(novoDocente);
+console.log(novoDocente.aprovarEstudante('Sofia', 'Javascript'));
+
